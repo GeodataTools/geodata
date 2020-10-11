7bfd83c0d8e52c4cc4fc9b487ee20076bab5853a
willhonaker <wcbhonaker@gmail.com>
2020-10-11 10:40:58 -0700

    fix close statements

--- geodata/preparation.py
+++ geodata/preparation.py
@@ -175,8 +175,8 @@ def cutout_prepare(cutout, overwrite=False, nprocesses=None, gebco_height=False)
 			ds.to_netcdf(fn)
 			ds.close()		# close xarray access before unlinking (Win32)
 			for tfn in fns: 
-				os.unlink(tfn)
 				os.close(tfn)
+				os.unlink(tfn)
 		logger.debug("Completed files %s", os.path.basename(fn))
 
 	logger.info("Cutout '%s' has been successfully prepared", cutout.name)
