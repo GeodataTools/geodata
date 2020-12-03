9c87ffcb35ba891a7d661bbdd0e7cec557bf921b
Michael Davidson <michd@mit.edu>
2020-12-03 15:54:06 -0800

    Create config-default.py

---
+++ geodata/config-default.py
@@ -0,0 +1,10 @@
+# Dataset Settings - Replace example paths with paths to your data.
+gebco_path = '/Users/geodata_example_path/gebco'
+cutout_dir = '/Users/geodata_example_path/cutouts'
+era5_dir = '/Users/geodata_example_path/era5'
+merra2_dir = '/Users/geodata_example_path/merra2'
+ncep_dir = '/Users/geodata_example_path/rda_ucar'
+cordex_dir = '/Users/geodata_example_path/cordex/RCP8.5'
+sarah_dir = '/Users/geodata_example_path/sarah_v2'
+# weather_dataset = {'module': 'cordex', 'model': 'MPI-M-MPI-ESM-LR'}
+weather_dataset = {'module': 'era5'}
