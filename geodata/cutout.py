0b18d929c0be2cb66b61c428298acbd0cbfdf468
j1feng <51950358+j1feng@users.noreply.github.com>
2022-02-09 15:39:37 -0800

    Resolve cutout.py conflicts between master/mask

--- geodata/cutout.py
+++ geodata/cutout.py
@@ -1,4 +1,5 @@
-## Copyright 2020 Michael Davidson (UCSD), William Honaker. Copyright 2016-2017 Gorm Andresen (Aarhus University), Jonas Hoersch (FIAS), Tom Brown (FIAS)
+## Copyright 2020 Michael Davidson (UCSD), William Honaker, Jiahe Feng (UCSD), Yuanbo Shi. 
+## Copyright 2016-2017 Gorm Andresen (Aarhus University), Jonas Hoersch (FIAS), Tom Brown (FIAS)
 
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -26,18 +27,22 @@ import xarray as xr
 import numpy as np
 import os, sys
 from six import string_types, itervalues, iteritems
+import pyproj
+import shapely
+from functools import partial
 
 import logging
 logger = logging.getLogger(__name__)
 
 from . import config, datasets
 
-from .convert import (convert_and_aggregate, heat_demand, temperature,
+from .convert import (convert_cutout, heat_demand, temperature,
 					  wind, pv, runoff, solar_thermal, soil_temperature)
 from .preparation import (cutout_do_task, cutout_prepare,
 						  cutout_produce_specific_dataseries,
 						  cutout_get_meta, cutout_get_meta_view)
 from .gis import compute_indicatormatrix
+from .mask import load_mask
 
 class Cutout(object):
 	def __init__(self, name=None, cutout_dir=config.cutout_dir, **cutoutparams):
@@ -48,6 +53,9 @@ class Cutout(object):
 		self.meta_append = 0
 		self.config = cutoutparams.pop('weather_data_config')
 		self.meta = meta = None
+		self.merged_mask = None
+		self.shape_mask = None
+		self.area = None
 
 
 		if 'bounds' in cutoutparams:
@@ -226,6 +234,123 @@ class Cutout(object):
 	def indicatormatrix(self, shapes, shapes_proj='latlong'):
 		return compute_indicatormatrix(self.grid_cells(), shapes, self.projection, shapes_proj)
 
+	## Masking
+
+	def add_mask(self, name, merged_mask = True, shape_mask = True):
+		"""
+		Add mask attribute to the cutout, from a previously saved mask objects.
+		The masks will be coarsened to the same dimension with the cutout metadata in Xarray.
+
+		name (str): name of the previously saved mask
+		merged_mask (bool): if true, the program will try to 
+			include the merged_mask from the mask object. Defaults to True.
+		shape_mask (bool): if true, the program will try to 
+			include the extracted dictionary of shape_mask from the mask object. Defaults to True.
+
+		"""
+		#make sure data is in correct format for coarsening
+		xr_ds = self.meta.reset_coords(['lon', 'lat'], drop = True).rename({'x': 'lon', 'y': 'lat'})
+		xr_ds = xr_ds.sortby(['lat', 'lon'])
+		mask = load_mask(name, mask_dir = config.mask_dir)
+
+		if not mask.merged_mask and not mask.shape_mask:
+			raise ValueError(f"No mask found in {mask.name}. Please create a proper mask object first.")
+
+		if mask.merged_mask and merged_mask:
+			self.merged_mask = coarsen(mask.load_merged_xr(), xr_ds)
+			logger.info("Cutout.merged_mask added.")
+
+		if mask.shape_mask and shape_mask:
+			self.shape_mask = {k: coarsen(v, xr_ds) for k, v in mask.load_shape_xr().items()}
+			logger.info("Cutout.shape_mask added.")
+
+	def add_grid_area(self, axis = ("lat", "lon"), adjust_coord = True):
+		"""
+		Add attribute 'area' to the cutout containing area for each grid cell 
+		in the cutout metedata Xarray.
+
+		axis (tuple): axis to include in the result xarray dataset. 
+			Defaults to ("lat", "lon").
+		adjust_coord (bool): sort the data by latitude and longitude values if true. 
+			Defaults to True.
+
+		"""
+		xr_ds = self.meta.reset_coords(['lon', 'lat'], drop = True).rename({'x': 'lon', 'y': 'lat'})
+		area_arr = np.zeros((xr_ds.lat.shape[0], xr_ds.lon.shape[0]))
+		lat_diff = np.abs((xr_ds.lat[1].values - xr_ds.lat[0].values))
+		for i, lat in enumerate(xr_ds.lat.values):
+			lat_bottom = lat - lat_diff/ 2
+			lat_top = lat + lat_diff/ 2
+			#calculate the area for grid cells with same latitude
+			area_arr[i] = np.round(calc_grid_area([
+							(xr_ds.lon.values[0], lat_top), 
+							(xr_ds.lon.values[0], lat_bottom),
+							(xr_ds.lon.values[1], lat_bottom), 
+							(xr_ds.lon.values[1], lat_top)]), 2)
+		if axis == ("lat", "lon"):
+			xr_ds = xr_ds.assign({"area": (axis, area_arr)})
+		elif axis == ("time", "lat", "lon"):
+			xr_ds = xr_ds.assign({"area": (axis, [area_arr])})
+
+		if adjust_coord:
+			if (('lon' not in xr_ds.coords) and ('lat' not in xr_ds.coords)
+				and ('x' in xr_ds.coords and 'y' in xr_ds.coords)):
+				xr_ds = xr_ds.rename({'x': 'lon', 'y': 'lat'})
+			xr_ds = xr_ds.sortby(['lat', 'lon'])
+
+		self.area = xr_ds
+	
+	def mask(self, dataset, true_area = True, 
+					merged_mask = True, shape_mask = True):
+		"""
+		Mask a converted Xarray dataSet from cutout with previously added mask attribute
+		with optional area inclusion, and return a dictionary of xarray Dataset.
+
+		The program will search for 'merged_mask' and 'shape_mask' attributes in the 
+		cutout object, these Xarray data can be generate through 'add_mask', unless the user 
+		specify 'merged_mask = False' or 'shape_mask = False', the masks in shape_mask 
+		will have the same key in the dictionary returned, and the mask for merged_mask will 
+		have the key name "merged_mask".
+
+		dataset (Xarray.DataSet): 
+		true_area (bool): if the returned masks will have the area variable. Defaults to True.
+		merged_mask (bool): if true, the program will try to 
+			include the merged_mask from the cutout object. Defaults to True.
+		shape_mask (bool): if true, the program will try to 
+			include the extracted dictionary of shape_mask from the cutout object. Defaults to True.
+
+		Returns: (dict): a dictionary with name keys and xarray DataSet with mask variable as values.
+		
+		"""
+		axis = ("lat", "lon")
+
+		if self.merged_mask is None and self.shape_mask is None:
+			raise ValueError(f"No mask found in cutout. Please add masks with self.add_mask()")
+
+		if self.area is None and true_area == True:
+			raise ValueError("No area data found. Please call self.add_grid_area() or set true_area to False.")
+
+		res = {}
+		
+		if self.merged_mask is not None and merged_mask:
+			ds = dataset.assign({"mask": (axis, self.merged_mask.data[0])})
+			if true_area:
+				ds = ds.assign({"area": (axis, self.area['area'].data)})
+			res['merged_mask'] = ds
+			logger.info("merged_mask combined with dataset. ")
+
+		if self.shape_mask is not None and shape_mask:
+			for key, val in self.shape_mask.items():
+				ds = dataset.assign({"mask": (axis, val.data[0])})
+				if true_area:
+					ds = ds.assign({"area": (axis, self.area['area'])})
+				res[key] = ds
+		logger.info("shape_mask combined with dataset. ")
+
+		return res
+		
+	
+
 	## Preparation functions
 
 	get_meta = cutout_get_meta    # preparation.cutout_get_meta
@@ -238,7 +363,7 @@ class Cutout(object):
 
 	## Conversion and aggregation functions
 
-	convert_and_aggregate = convert_and_aggregate
+	convert_cutout = convert_cutout
 
 	heat_demand = heat_demand
 
@@ -252,3 +377,94 @@ class Cutout(object):
 
 	pv = pv
 
+
+
+def _find_intercept(list1, list2, start, threshold = 0):
+	'''
+	find_intercept is a helper function to find the best start point for doing coarsening 
+	in order to make the coordinates of the coarsen as close to the target as possible.
+	'''
+	min_pos = 0
+	min_res = 0
+	init = 0
+	for i in range(len(list1)-start):
+		resid = ((list1[start+i] -list2[0]) % (list2[1] - list2[0])).values.tolist()
+		if i == 0:
+			init = resid
+		if resid <= threshold:
+			return i
+		if resid > min_res:
+			min_res = resid
+			min_pos = i
+		else:
+			min_res = resid
+			break
+	if min_res == init:
+		return 0
+	else:
+		return i
+
+def coarsen(ori, tar, threshold = 0, func = 'mean'):
+	'''
+	This function will reindex the original xarray dataset according to the coordiantes of the target.
+	
+	There might be a bias for lattitudes and longitudes. The bias are normally within 0.01 degrees. 
+	In order to not lose too much data, a threshold for bias in degree could be given. 
+	When threshold = 0, it means that the function is going to find the best place with smallest bias.
+	
+	'''
+	lat_multiple = round(((tar.lat[1] - tar.lat[0]) / (ori.lat[1] - ori.lat[0])).values.tolist())
+	lon_multiple = round(((tar.lon[1] - tar.lon[0]) / (ori.lon[1] - ori.lon[0])).values.tolist())
+	
+	lat_start = _find_intercept(ori.lat, tar.lat, (lat_multiple - 1) //  2) 
+	lon_start = _find_intercept(ori.lon, tar.lon, (lon_multiple - 1) //  2)
+	
+	if func == 'mean':
+		coarsen = ori.isel(lat = slice(lat_start, None), lon = slice(lon_start, None)).coarsen(
+			dim = {'lat': lat_multiple, 'lon' : lon_multiple}, 
+			side = {'lat': 'left', 'lon': 'left'}, 
+			boundary = 'pad', ).mean()
+	elif func == 'sum':
+		coarsen = ori.isel(lat = slice(lat_start, None), lon = slice(lon_start, None)).coarsen(
+			dim = {'lat': lat_multiple, 'lon' : lon_multiple}, 
+			side = {'lat': 'left', 'lon': 'left'}, 
+			boundary = 'pad', ).sum()
+	out = coarsen.reindex_like(tar, method = 'nearest')
+	return out	
+
+def calc_grid_area(lis_lats_lons):
+	"""
+	Calculate area in km^2 for a grid cell given lats and lon border, with help from:
+	https://stackoverflow.com/questions/4681737/how-to-calculate-the-area-of-a-polygon-on-the-earths-surface-using-python
+
+	"""
+	lons, lats = zip(*lis_lats_lons)
+	ll = list(set(lats))[::-1]
+	var = []
+	for i in range(len(ll)):
+		var.append('lat_' + str(i+1))
+	st = ""
+	for v, l in zip(var,ll):
+		st = st + str(v) + "=" + str(l) +" "+ "+"
+	st = st +"lat_0="+ str(np.mean(ll)) + " "+ "+" + "lon_0" +"=" + str(np.mean(lons))
+	tx = "+proj=aea +" + st
+	pa = pyproj.Proj(tx)
+
+	x, y = pa(lons, lats)
+	cop = {"type": "Polygon", "coordinates": [zip(x, y)]}
+
+	return shapely.geometry.shape(cop).area/1000000
+
+def calc_shp_area(shp, shp_projection = '+proj=latlon'):
+	"""calculate area in km^2 of the shapes for each shp object"""
+	temp_shape =  shapely.ops.transform(partial(
+            pyproj.transform,
+            pyproj.Proj(shp_projection),
+            pyproj.Proj(
+                proj='aea',
+                lat_1 = shp.bounds[1],
+                lat_2 = shp.bounds[3]
+            )
+        ),
+    shp)
+	return temp_shape.area / 1000000
