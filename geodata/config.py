c71481667e3f1d9e179e29099dbffc5a0ee9edb1
Xiqiang Liu <9440183+xiqiangliu@users.noreply.github.com>
2022-10-13 11:25:41 -0700

    Make config of dataset & cutout locations optional

---
+++ geodata/config.py
@@ -0,0 +1,41 @@
+## Copyright 2022 Michael Davidson (UCSD)
+
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from pathlib import Path
+
+from geodata import ROOT_PATH
+
+DATASET_ROOT_PATH = Path(os.environ.get("GETDATA_ROOT", ROOT_PATH)) / "datasets"
+
+gebco_path = DATASET_ROOT_PATH / "gebco"
+cutout_dir = DATASET_ROOT_PATH / "cutouts"
+mask_dir = DATASET_ROOT_PATH / "masks"
+era5_dir = DATASET_ROOT_PATH / "era5"
+merra2_dir = DATASET_ROOT_PATH / "merra2"
+
+# Check if these paths exists
+for path in [gebco_path, cutout_dir, mask_dir, era5_dir, merra2_dir]:
+    if not path.is_dir():
+        path.mkdir(exist_ok=True, parents=True)
+
+gebco_path = str(gebco_path)
+cutout_dir = str(cutout_dir)
+mask_dir = str(mask_dir)
+era5_dir = str(era5_dir)
+merra2_dir = str(merra2_dir)
+
+# weather_dataset = {'module': 'cordex', 'model': 'MPI-M-MPI-ESM-LR'}
+weather_dataset = {"module": "era5"}
