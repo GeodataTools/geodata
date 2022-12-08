492c02cc417873805c64d669bdc5237c16d86dd6
Xiqiang Liu <9440183+xiqiangliu@users.noreply.github.com>
2022-12-08 11:23:46 -0800

    chore: add copyright

--- src/geodata/datasets/era5.py
+++ src/geodata/datasets/era5.py
@@ -1,5 +1,5 @@
-## Copyright 2016-2017 Jonas Hoersch (FIAS), Tom Brown (FIAS), Markus Schlott
-## (FIAS)
+## Copyright 2016-2017 Jonas Hoersch (FIAS), Tom Brown (FIAS), Markus Schlott (FIAS)
+## Copyright 2022 Xiqiang Liu
 
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
