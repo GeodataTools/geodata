c40ee81bfa5ce723044d19dcb9e622eae1b85123
Xiqiang Liu <9440183+xiqiangliu@users.noreply.github.com>
2023-04-29 12:29:51 +0800

    feat: use modern tqdm instead of progressbar2

--- src/geodata/convert.py
+++ src/geodata/convert.py
@@ -20,20 +20,197 @@ This module contains various functions used to perform conversion in Geodata.
 
 import logging
 from operator import itemgetter
-from typing import TypeVar
 
 import numpy as np
 import xarray as xr
+from six import string_types
+from tqdm.auto import tqdm
 
 from . import wind as windm
 from .pv.irradiation import TiltedIrradiation
 from .pv.orientation import SurfaceOrientation, get_orientation  # noqa: F401
 from .pv.solar_panel_model import SolarPanelModel
 from .pv.solar_position import SolarPosition
-from .resource import get_solarpanelconfig, get_windturbineconfig, windturbine_smooth  # noqa: F401
+from .resource import get_solarpanelconfig, get_windturbineconfig, windturbine_smooth
 
 logger = logging.getLogger(__name__)
-Cutout = TypeVar("Cutout")
+
+
+def convert_cutout(cutout, convert_func, show_progress=False, **convert_kwds):
+    """
+    Convert and aggregate a weather-based renewable generation time-series.
+
+    NOTE: Not meant to be used by the user him or herself. Rather it is a
+    gateway function that is called by all the individual time-series
+    generation functions like pv and wind. Thus, all its parameters are also
+    available from these.
+
+    Parameters (passed through as **params)
+    ---------------------------------------
+    show_progress : boolean|string
+            Whether to show a progress bar if boolean and its label if given as a
+            string (defaults to True).
+
+    Returns
+    -------
+    resource : xr.DataArray
+            Time-series of renewable generation aggregated to buses, if
+            `matrix` or equivalents are provided else the total sum of
+            generated energy.
+
+    Internal Parameters (provided by f.ex. wind and pv)
+    ---------------------------------------------------
+    convert_func : Function
+            Callback like convert_wind, convert_pv
+    """
+    if not cutout.prepared:
+        raise RuntimeError("The cutout has to be prepared first.")
+
+    results = []
+
+    yearmonths = cutout.coords["year-month"].to_index()
+
+    if isinstance(show_progress, string_types):
+        prefix = show_progress
+    else:
+        func_name = (
+            convert_func.__name__[len("convert_") :]
+            if convert_func.__name__.startswith("convert_")
+            else convert_func.__name__
+        )
+        prefix = f"Convert `{func_name}`: "
+
+    pbar = tqdm if show_progress else lambda x, desc: x
+    for ym in pbar(yearmonths, desc=prefix):
+        with xr.open_dataset(cutout.datasetfn(ym)) as ds:
+            if "view" in cutout.meta.attrs:
+                if isinstance(cutout.meta.attrs["view"], str):
+                    cutout.meta.attrs["view"] = {}
+                    cutout.meta.attrs.setdefault("view", {})["x"] = slice(
+                        min(cutout.meta.coords["x"]).values.tolist(),
+                        max(cutout.meta.coords["x"]).values.tolist(),
+                    )
+                    cutout.meta.attrs.setdefault("view", {})["y"] = slice(
+                        min(cutout.meta.coords["y"]).values.tolist(),
+                        max(cutout.meta.coords["y"]).values.tolist(),
+                    )
+                ds = ds.sel(**cutout.meta.attrs["view"])
+
+            da = convert_func(ds, **convert_kwds)
+            results.append(da.load())
+
+    results = xr.concat(results, dim="time")
+
+    return results
+
+
+## temperature
+
+
+def convert_temperature(ds):
+    """Return outside temperature (useful for e.g. heat pump T-dependent
+    coefficient of performance).
+    """
+
+    # Temperature is in Kelvin
+    return ds["temperature"] - 273.15
+
+
+def temperature(cutout, **params):
+    return cutout.convert_cutout(convert_func=convert_temperature, **params)
+
+
+## soil temperature
+
+
+def convert_soil_temperature(ds):
+    """Return soil temperature (useful for e.g. heat pump T-dependent
+    coefficient of performance).
+    """
+
+    # Temperature is in Kelvin
+
+    # There are nans where there is sea; by setting them
+    # to zero we guarantee they do not contribute when multiplied
+    # by matrix in geodata/aggregate.py
+    return (ds["soil temperature"] - 273.15).fillna(0.0)
+
+
+def soil_temperature(cutout, **params):
+    return cutout.convert_cutout(convert_func=convert_soil_temperature, **params)
+
+
+## heat demand
+
+
+def convert_heat_demand(ds, threshold, a, constant, hour_shift):
+    # Temperature is in Kelvin; take daily average
+    T = ds["temperature"]
+    T.coords["time"].values += np.timedelta64(dt.timedelta(hours=hour_shift))
+
+    T = ds["temperature"].resample(time="1D").mean(dim="time")
+    threshold += 273.15
+    heat_demand_value = a * (threshold - T)
+
+    heat_demand_value.values[heat_demand_value.values < 0.0] = 0.0
+
+    return constant + heat_demand_value
+
+
+def heat_demand(cutout, threshold=15.0, a=1.0, constant=0.0, hour_shift=0.0, **params):
+    """
+    Convert outside temperature into daily heat demand using the
+    degree-day approximation.
+
+    Since "daily average temperature" means different things in
+    different time zones and since xarray coordinates do not handle
+    time zones gracefully like pd.DateTimeIndex, you can provide an
+    hour_shift to redefine when the day starts.
+
+    E.g. for Moscow in winter, hour_shift = 4, for New York in winter,
+    hour_shift = -5
+
+    This time shift applies across the entire spatial scope of ds for
+    all times. More fine-grained control will be built in a some
+    point, i.e. space- and time-dependent time zones.
+
+    WARNING: Because the original data is provided every month, at the
+    month boundaries there is untidiness if you use a time shift. The
+    resulting xarray will have duplicates in the index for the parts
+    of the day in each month at the boundary. You will have to
+    re-average these based on the number of hours in each month for
+    the duplicated day.
+
+    Parameters
+    ----------
+    threshold : float
+            Outside temperature in degrees Celsius above which there is no
+            heat demand.
+    a : float
+            Linear factor relating heat demand to outside temperature.
+    constant : float
+            Constant part of heat demand that does not depend on outside
+            temperature (e.g. due to water heating).
+    hour_shift : float
+            Time shift relative to UTC for taking daily average
+
+    Note
+    ----
+    You can also specify all of the general conversion arguments
+    documented in the `convert_cutout` function.
+    """
+
+    return cutout.convert_cutout(
+        convert_func=convert_heat_demand,
+        threshold=threshold,
+        a=a,
+        constant=constant,
+        hour_shift=hour_shift,
+        **params,
+    )
+
+
+## solar thermal collectors
 
 
 def convert_solar_thermal(
