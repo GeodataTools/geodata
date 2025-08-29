165b234800ef9aa927b83021b41b26c007dc8f92
billchen24 <bill1317342672@gmail.com>
2025-08-28 17:34:02 -0700

    [fix] misnamed function

--- src/geodata/pvlib.py
+++ src/geodata/pvlib.py
@@ -524,7 +524,7 @@ def _prepare_pvlib_ds(cutout, *varnames):
     relative_humidity = _calculate_relative_humidity(
         temperature_celsius,
         #_convert_celsius(ds.dewpoint_temperature),
-        convert_celsius(ds.d2m),
+        _convert_celsius(ds.d2m),
     )
 
     precipitable_water = _calculate_precipitable_water(
